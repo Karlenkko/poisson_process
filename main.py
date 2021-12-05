bacc7dc6bc716c75f4fdc94c20e5f22d4e2412d3
Karlenkko <48716383+Karlenkko@users.noreply.github.com>
2021-12-06 00:01:03 +0100

    simulation in 1 day

--- main.py
+++ main.py
@@ -5,13 +5,26 @@ import matplotlib.pyplot as plt
 
 def poisson_process(n):
     p = random.poisson(lam=300, size=n)
-    print(np.average(p))
+    plt.hist(p)
+    plt.title("simulation in 10 years")
+    plt.show()
     return p
+
+def one_day():
+    p = random.exponential(1/300, 6000)
+    sum = 0
+    idx = 0
+    for i in p:
+        idx += 1
+        sum += i
+        if sum > 1:
+            break
+    plt.hist(p[:idx])
+    print(idx)
+    plt.title("arrival in 1 day")
+    plt.show()
 
 
 if __name__ == '__main__':
-    plt.plot(range(100), poisson_process(100))
-    plt.title("simulation of poisson process")
-    plt.xlabel("Days")
-    plt.ylabel("number of visitors")
-    plt.show()
+    # poisson_process(3650)
+    one_day()
